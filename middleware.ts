8289b3bcfcace1c4e7a360d948c807acab019e27
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2025-01-22 21:24:06 +0530

    feat: add middleware for route authorization
    - Created a new file `middleware.ts` to handle route authorization using NextAuth middleware.
    - Implemented `withAuth` middleware function to allow or deny requests based on user roles and paths.
    - Added `authorized` callback to define authorization rules:
      - Allowed access to authentication routes (`/api/auth`, `/login`, `/register`).
      - Allowed access to public routes (`/`, `/api/products`, `/products`).
      - Restricted access to admin routes (`/admin`) to users with an 'admin' role.
      - Allowed access to other routes for authenticated users.
    - Configured the middleware to match all request paths except for:
      - `_next/static` (static files)
      - `_next/image` (image optimization files)
      - `favicon.ico` (favicon file)
      - `/public` (public folder)
    
    This commit introduces a robust authorization system to control access to various routes based on user roles, enhancing the security and organization of the application.

---
+++ middleware.ts
@@ -0,0 +1,59 @@
+import { NextResponse } from 'next/server'
+import { withAuth } from 'next-auth/middleware'
+
+
+export default withAuth(
+
+    function middleware() {
+        // NOTE - Allow the request
+        return NextResponse.next()
+    },
+    {
+        callbacks: {
+            authorized: ({ token, req }) => {
+                // NOTE - Get user path-name
+                const { pathname } = req.nextUrl
+
+                // NOTE - Check for API routes -- [Authentication routes]
+                if (
+                    pathname.startsWith('/api/auth') ||
+                    pathname === '/login' ||
+                    pathname === '/register'
+                ) {
+                    return true
+                }
+
+                // NOTE - Check for [Public routes]
+                if (
+                    pathname === '/' ||
+                    pathname.startsWith('/api/products') ||
+                    pathname.startsWith('/products')
+                ) {
+                    return true
+                }
+
+                // NOTE - Check for [Admin routes]
+                if (pathname.startsWith('/admin')) {
+                    return token?.role === 'admin'
+                }
+
+
+                // NOTE - Check for Authenticated [Other routes]
+                return !!token
+            }
+        },
+    }
+)
+
+export const config = {
+    matcher: [
+        /*
+            * Match all request paths except for the ones starting with:
+            * - _next/static (static files)
+            * - _next/image (image optimization files)
+            * - favicon.ico (favicon file)
+            * - /public (public folder)
+        */
+        "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+    ]
+}
