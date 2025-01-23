4d9d1217018ffe8210d90ce5857d584be74aeac1
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2025-01-23 16:08:19 +0530

    feat: add Providers component to manage ImageKit and NextAuth contexts
    - Created a new file `Providers.tsx` under `app/components` to wrap the application with necessary providers.
    - Imported `ImageKitProvider` and `IKImage` from `imagekitio-next` to manage ImageKit configuration.
    - Imported `SessionProvider` from `next-auth/react` to manage authentication sessions.
    - Defined `urlEndpoint` and `publicKey` using environment variables for ImageKit configuration.
    - Implemented an `authenticator` function to fetch authentication parameters from the `/api/imagekit-auth` endpoint.
      - Handled errors and ensured proper response handling.
    - Returned a component that wraps its children with both `SessionProvider` and `ImageKitProvider`, ensuring that both contexts are available throughout the application.
    
    This commit integrates ImageKit and NextAuth providers, enabling image handling and authentication management across the application.

---
+++ app/components/Providers.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import React from "react";
+import { ImageKitProvider, IKImage } from "imagekitio-next";
+import { SessionProvider } from 'next-auth/react';
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY!;
+
+
+export default function Providers(
+    { children }: { children: React.ReactNode }
+) {
+    const authenticator = async function () {
+        try {
+            // NOTE - Get Respone from the API_Endpoint
+            const response = await fetch('/api/imagekit-auth');
+
+            // NOTE - Check if Response is not Ok
+            if (!response.ok) {
+                throw new Error('Failed to authenticate');
+            }
+
+            // NOTE - Return Response
+            return response.json();
+
+
+        } catch (error: any) {
+            console.error('Error authenticating:', error);
+            throw new Error('Failed to authenticate');
+        }
+    };
+
+
+    // NOTE - Return Provider
+    return (
+        < SessionProvider refetchInterval={5 * 60}>
+            <ImageKitProvider
+                urlEndpoint={urlEndpoint}
+                publicKey={publicKey}
+                authenticator={authenticator}
+            >
+                {children}
+            </ImageKitProvider>
+        </SessionProvider>
+    );
+}
